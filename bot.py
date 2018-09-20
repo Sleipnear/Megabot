ebe211b20fc165cdce138116bd164d76ab0e070e
Dmitrii Zaikin <sleipnear@gmail.com>
2018-09-20 20:27:00 +0500

    домисал с модулем ephem

--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import settings
+import ephem
 
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+
+'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
                     )
 
+teleg_log=logging.getLogger('telegram.ext.updater')
 
 def greet_user(bot, update):
     text = 'Вызван /start'
@@ -20,6 +25,11 @@ def talk_to_me(bot, update):
 					update.message.chat.id, update.message.text)
 	update.message.reply_text(user_text)
 
+	def ephems(bot,update):
+    text = 'Вызван /ephem'
+    print(text)
+	update.message.reply_text(text)
+
 def main():
 	mybot = Updater(settings. API_KEY, request_kwargs=settings.PROXY)
 	logging.info('бот запускается')
@@ -27,6 +37,7 @@ def main():
 	dp = mybot.dispatcher
 	dp.add_handler(CommandHandler('start', greet_user))
 	dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+	dp.add_handler(CommandHandler("ephem", ephems))
 
 	mybot.start_polling()
 	mybot.idle()
